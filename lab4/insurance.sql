7fb6d515d7cb3bdabda68dc78ae9b057cd1e6ffb
Chethan K <143086835+Cheth2107@users.noreply.github.com>
2023-12-10 13:37:54 +0530

    Update insurance.sql

--- lab4/insurance.sql
+++ lab4/insurance.sql
@@ -136,5 +136,76 @@ D04	ROCK	Jayanagar
 
 SELECT * FROM car WHERE model LIKE 'l%';
 
-R02	limo	2007
-R03	lamborgini	2007
+R02	limo 2007
+R03	lamborgini 2007
+
+ALTER TABLE car
+UPDATE year = 2015
+WHERE regno = R04;
+
+ALTER TABLE car
+UPDATE year = 2014
+WHERE regno = R02;
+
+SELECT * FROM car;
+
+R01	ferrari	2007
+R02	limo	2014
+R03	lamborgini 2007
+R04	mercedes 2015
+R05	porche	2007
+
+SELECT * FROM car WHERE year BETWEEN 2006 AND 2010;
+
+R01	ferrari	2007
+R03	lamborgini 2007
+R05	porche	2007
+
+SELECT * FROM car WHERE year NOT BETWEEN 2006 AND 2010;
+
+R02	limo	2014
+R04	mercedes 2015
+
+SELECT * FROM participated WHERE damage_amt>7000;
+
+D03	R03	119	7500
+D04	R04	110	8500
+D05	R05	120	9500
+
+SELECT * FROM participated WHERE damage_amt<7000;
+
+D01	R01	117	5500
+D02	R02	118	6500
+
+SELECT * FROM person ORDER BY name ASC;
+
+D03	JACK	Vijaynagar
+D02	JAMES	RK Nagar
+D01	JOHN	hebbal
+D05	PETER	VV Puram
+D04	ROCK	Jayanagar
+
+SELECT * FROM participated ORDER BY damage_amt DESC;
+
+D05	R05	120	9500
+D04	R04	110	8500
+D03	R03	119	7500
+D02	R02	118	6500
+D01	R01	117	5500
+
+SELECT * FROM accident ORDER BY location ASC;
+
+117	2023-05-11	street A
+118	2023-05-13	street B
+119	2023-05-14	street C
+110	2023-05-15	street D
+120	2023-05-12	street E
+
+SELECT * FROM accident ORDER BY acc_date DESC;
+
+110	2023-05-15	street D
+119	2023-05-14	street C
+118	2023-05-13	street B
+120	2023-05-12	street E
+117	2023-05-11	street A
+
