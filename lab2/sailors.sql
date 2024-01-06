678d0fe3b4540d07cade717982116b5820d59ae4
Chethan K <143086835+Cheth2107@users.noreply.github.com>
2024-01-06 19:20:08 +0530

    Update sailors.sql

--- lab2/sailors.sql
+++ lab2/sailors.sql
@@ -82,7 +82,7 @@ select * from Sailors;
 select * from Boat;
 select * from reserves;
 
--- Find the colours of the boats reserved by Albert
+--1. Find the colours of the boats reserved by Albert
 select color 
 from Sailors s, Boat b, reserves r 
 where s.sid=r.sid and b.bid=r.bid and s.sname="Albert";
@@ -91,7 +91,7 @@ Blue
 Red
 Green
 
--- Find all the sailor sids who have rating atleast 8 or reserved boat 103
+--2. Find all the sailor sids who have rating atleast 8 or reserved boat 103
 
 (select sid from Sailors where Sailors.rating>=8)
 UNION
@@ -100,7 +100,7 @@ UNION
 3
 5
 
--- Find the names of the sailor who have not reserved a boat whose name contains the string "storm". Order the name in the ascending order
+--3. Find the names of the sailor who have not reserved a boat whose name contains the string "storm". Order the name in the ascending order
 
 select s.sname from Sailors s
 where s.sid not in 
@@ -110,7 +110,7 @@ order by s.sname ASC;
 
 Astorm Gowda
   
--- Find the name of the sailors who have reserved all boats
+--4. Find the name of the sailors who have reserved all boats
 
 select sname from Sailors s where not exists
 	(select * from Boat b where not exists
@@ -118,7 +118,7 @@ select sname from Sailors s where not exists
 
 Albert
 
--- Find the name and age of the oldest sailor
+--5. Find the name and age of the oldest sailor
 
 select sname, age
 from Sailors where age in (select max(age) from Sailors);
@@ -129,7 +129,7 @@ from Sailors where age in (select max(age) from Sailors);
 | Astorm Gowda |  68 |
 +--------------+-----+
 
-  -- For each boat which was reserved by atleast 2 sailors with age >= 40, find the bid and average age of such sailors
+  --6. For each boat which was reserved by atleast 2 sailors with age >= 40, find the bid and average age of such sailors
 
   SELECT b.bid, AVG(s.age) AS average_age
 FROM Sailors s, Boat b, reserves r
@@ -143,45 +143,7 @@ HAVING 2 <= COUNT(DISTINCT r.sid);
 | 103 |        54.5 |
 +-----+-------------+
 
--- A view that shows names and ratings of all sailors sorted by rating in descending order\
-
-CREATE VIEW NamesAndRating AS
-SELECT sname, rating
-FROM Sailors
-ORDER BY rating DESC;
-
-SELECT * FROM NamesAndRating;
-
-+--------------+--------+
-| sname        | rating |
-+--------------+--------+
-| Darshan      | 9      |
-| Armstormin   | 7      |
-| Albert       | 5      |
-| Nakul        | 5      |
-| Astorm Gowda | 2      |
-+--------------+--------+
-
-
--- Create a view that shows the names of the sailors who have reserved a boat on a given date.
-
-  CREATE VIEW SailorsWithReservation AS
-SELECT s.sname
-FROM Sailors s, reserves r
-WHERE r.sid = s.sid AND r.sdate = "2023-03-06";
-
-SELECT * FROM SailorsWithReservation;
-
-
-+--------------+
-| sname        |
-+--------------+
-| Albert       |
-| Armstormin   |
-| Astorm Gowda |
-+--------------+
-
-  -- Create a view that shows the names and colours of all the boats that have been reserved by a sailor with a specific rating.
+--7. Create a view that shows the names and colours of all the boats that have been reserved by a sailor with a specific rating.
 
 CREATE VIEW ReservedBoatsWithRatedSailor AS
 SELECT DISTINCT b.bname, b.color
@@ -198,7 +160,7 @@ SELECT * FROM ReservedBoatsWithRatedSailor;
 +-------+-------+
 
 
-  -- Trigger that prevents boats from being deleted if they have active reservation
+  --8. Trigger that prevents boats from being deleted if they have active reservation
   
 DELIMITER //
 CREATE OR REPLACE TRIGGER CheckAndDelete
@@ -213,38 +175,3 @@ END;
 
 DELIMITER ;
 delete from Boat where bid=103;-- This gives error since boat 103 is reserved
-
-
--- A trigger that prevents sailors with rating less than 3 from reserving a boat.
-
-
-DELIMITER //
-create trigger BlockReservation
-before insert on reserves
-for each row
-BEGIN
-	IF EXISTS (select * from Sailors where sid=new.sid and rating<3) THEN
-		signal sqlstate '45000' set message_text='Sailor rating less than 3';
-	END IF;
-END;//
-
-DELIMITER ;
-
-insert into reserves values
-(4,2,"2023-10-01");
-
-
-DELIMITER //
-
-CREATE TRIGGER prevent_boat_delete
-BEFORE DELETE ON Boats
-FOR EACH ROW
-BEGIN
-    IF EXISTS (SELECT 1 FROM Reservations WHERE BoatID = OLD.BoatID AND Status = 'Active') THEN
-        SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'Error: Cannot delete a boat with active reservations.';
-    END IF;
-END;
-//
-
-DELIMITER ;
