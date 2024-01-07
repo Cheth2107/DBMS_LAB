4bfc795764e68f11a15b73cecdc7402a0b53282d
Chethan K <143086835+Cheth2107@users.noreply.github.com>
2024-01-07 22:14:51 +0530

    Update order_processing.sql

--- order_processing.sql
+++ order_processing.sql
@@ -111,8 +111,6 @@ delete from Orders where cust_id = (select cust_id from Customers where cname li
 --5. Find the item with the maximum unit price.
 select max(unitprice) from Items;
 
-
-
 --6. A tigger that updates order_amount based on quantity and unit price of order_item
 
 DELIMITER $$
@@ -120,10 +118,12 @@ create trigger UpdateOrderAmt
 after insert on OrderItems
 for each row
 BEGIN
-	update Orders set order_amt=(new.qty*(select distinct unitprice from Items NATURAL JOIN OrderItems where item_id=new.item_id)) where Orders.order_id=new.order_id;
-END; $$
-DELIMITER ;
+update Orders set order_amt=(new.qty*(select distinct unitprice from Items NATURAL JOIN OrderItems where item_id=new.item_id))
+where Orders.order_id=new.order_id;
+END; 
+$$
 
+DELIMITER ;
 INSERT INTO Orders VALUES
 (006, "2020-12-23", 0004, 1200);
 
