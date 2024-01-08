04cad2dba2e8c77f9f744d34ddb3f46cdc4f53e1
Chethan K <143086835+Cheth2107@users.noreply.github.com>
2024-01-08 08:14:45 +0530

    Update company.sql

--- company.sql
+++ company.sql
@@ -44,7 +44,7 @@ create table if not exists WorksOn(
 );
 
 INSERT INTO Employee VALUES
-("01NB235", "Chandan_Krishna","Siddartha Nagar, Mysuru", "Male", 1500000, "01NB235", 5),
+("01NB235", "Daisy_Scott","Hebbal, Mysuru", "Female", 1500000, "01NB235", 5),
 ("01NB354", "Employee_2", "Lakshmipuram, Mysuru", "Female", 1200000,"01NB235", 2),
 ("02NB254", "Employee_3", "Pune, Maharashtra", "Male", 1000000,"01NB235", 4),
 ("03NB653", "Employee_4", "Hyderabad, Telangana", "Male", 2500000, "01NB354", 5),
@@ -52,11 +52,11 @@ INSERT INTO Employee VALUES
 
 
 INSERT INTO Department VALUES
-(001, "Human Resources", "01NB235", "2020-10-21"),
-(002, "Quality Assesment", "03NB653", "2020-10-19"),
-(003,"System assesment","04NB234","2020-10-27"),
-(005,"Production","02NB254","2020-08-16"),
-(004,"Accounts","01NB354","2020-09-4");
+(001, "Human Resources", "01NB235", "2024-10-21"),
+(002, "Quality Assesment", "03NB653", "2024-10-19"),
+(003,"System assesment","04NB234","2024-10-27"),
+(005,"Production","02NB254","2024-08-16"),
+(004,"Accounts","01NB354","2024-09-4");
 
 
 INSERT INTO DLocation VALUES
@@ -91,7 +91,7 @@ SELECT * FROM WorksOn;
 
 --1. Make a list of all project numbers for projects that involve an employee whose last name is ‘Scott’, either as a worker or as a manager of the department that controls the project.
 
-select p_no,p_name,name from Project p, Employee e where p.d_no=e.d_no and e.name like "%Krishna";
+select p_no,p_name,name from Project p, Employee e where p.d_no=e.d_no and e.name like "%Scott";
 
 
 --2. Show the resulting salaries if every employee working on the ‘IoT’ project is given a 10 percent raise
@@ -125,11 +125,11 @@ create trigger PreventDelete
 before delete on Project
 for each row
 BEGIN
-	IF EXISTS (select * from WorksOn where p_no=old.p_no) THEN
-		signal sqlstate '45000' set message_text='This project has an employee assigned';
-	END IF;
-END; //
+IF EXISTS (select * from WorksOn where p_no=old.p_no) THEN
+signal sqlstate '45000' set message_text='This project has an employee assigned';
+END IF;
+END; 
+//
 
 DELIMITER ;
-
 delete from Project where p_no=241563; -- Will give error 
